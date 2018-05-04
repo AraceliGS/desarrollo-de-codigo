1da0e04d8840d387c5cd58b7f4dc31d3c0c18f17
AraceliGS <aragutarras@gmail.com>
2018-05-04 18:16:49 -0500

    obteniendo el menor y mayor número de mi arreglo

--- reto-1/js/app.js
+++ reto-1/js/app.js
@@ -1,1 +1,29 @@
-// alert('La página se cargó correctamente.');
+let button = document.getElementById('get-answer');
+let maxNumberSpan = document.getElementById('max-number');
+let minNumberSpan = document.getElementById('min-number');
+let secondMaxNumberSpan = document.getElementById('second-max-number');
+
+// Evento click
+button.addEventListener('click', function() {
+  // console.log(button);
+  let numbers = [45, 75, 200, 256, 1, 27];
+  let MAX = numbers[0];
+  let MIN = numbers[0];
+  // Recorriendo el array
+  for(var i = 0; i < numbers.length ; i++) {
+    // debugger;
+    console.log(numbers[i]);
+    // Comparando
+    if(numbers[i] > MAX) {
+      MAX = numbers[i];
+    } else if (numbers[i] < MAX && numbers[i] <= MIN) {
+      MIN = numbers[i];
+    } else {
+      console.log('Este número no es el menor ni el mayor.');
+      MIN = MIN;
+      MAX = MAX;
+    }
+  }
+  maxNumberSpan.textContent = MAX;
+  minNumberSpan.textContent= MIN;
+});
