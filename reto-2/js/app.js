e8dd4a49751bf515732f5b0d79386aecb40666a6
AraceliGS <aragutarras@gmail.com>
2018-05-04 23:55:24 -0500

    añadiendo avances del segundo reto

--- reto-2/js/app.js
+++ reto-2/js/app.js
@@ -1,1 +1,17 @@
-
+window.addEventListener('load', function() {
+  // Variables
+  let button = document.getElementById('get-solutions');
+  let longestNameSpan= document.getElementById('longest-name')
+  button.addEventListener('click', function() {
+    // console.log('Hicistes click');
+    let names = ['erik', 'leonardo', 'andres', 'gerardo', 'jan', 'renzo'];
+    let namesLength = [];
+    for(var i = 0; i < names.length; i++) {
+      let letters = names[i].split('');
+      console.log(letters);
+      for(var i = 0; i < letters.length; i++) {
+        
+      };
+    };
+  });
+});
