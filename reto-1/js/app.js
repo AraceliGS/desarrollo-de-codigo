3fa4f425e711c5437b7dcc6f30873ac5cc8608d4
AraceliGS <aragutarras@gmail.com>
2018-05-04 22:26:47 -0500

    modificando diseño de la página web

--- reto-1/js/app.js
+++ reto-1/js/app.js
@@ -1,37 +1,44 @@
-// Variables
-let button = document.getElementById('get-answer');
-let maxNumberSpan = document.getElementById('max-number');
-let minNumberSpan = document.getElementById('min-number');
-let secondMaxNumberSpan = document.getElementById('second-max-number');
-// Evento click
-button.addEventListener('click', function() {
-  // console.log(button);
-  let numbers = [45, 75, 200, 256, 1, 27];
-  let MAX = numbers[0];
-  let SECONDMAX = numbers[0];
-  let MIN = numbers[0];
-  // Recorriendo el array
-  for(var i = 0; i < numbers.length ; i++) {
-    debugger;
-    console.log(numbers[i]);
-    // Comparando
-    if(numbers[i] > MAX) {
-      SECONDMAX = MAX;
-      MAX = numbers[i];
-    } else if(numbers[i] < MAX && numbers[i] > SECONDMAX) {
-      SECONDMAX = numbers[i];
-    } else if (numbers[i] < MAX && numbers[i] < SECONDMAX && numbers[i] <= MIN) {
-      MIN = numbers[i];
-    } else {
-      console.log('Este número no es el menor ni el mayor.');
-      MIN = MIN;
-      MAX = MAX;
-      SECONDMAX = SECONDMAX;
-      console.log(SECONDMAX);
+window.addEventListener('load', function() {
+  // Variables
+  let button = document.getElementById('get-answer');
+  let maxNumberSpan = document.getElementById('max-number');
+  let minNumberSpan = document.getElementById('min-number');
+  let secondMaxNumberSpan = document.getElementById('second-max-number');
+  // Función orderNumbers
+  function orderNumbers(arrayOfNumbers) {
+    // console.log(button);
+    let numbers = arrayOfNumbers;
+    let max = numbers[0];
+    let secondMax = numbers[0];
+    let min = numbers[0];
+    // Recorriendo el array
+    for(var i = 0; i < numbers.length ; i++) {
+      debugger;
+      console.log(numbers[i]);
+      // Comparando
+      if(numbers[i] > max) {
+        secondMax = max;
+        max = numbers[i];
+      } else if(numbers[i] < max && numbers[i] > secondMax) {
+        secondMax = numbers[i];
+      } else if (numbers[i] < max && numbers[i] < secondMax && numbers[i] <= min) {
+        min = numbers[i];
+      } else {
+        console.log('Este número no es el menor ni el mayor.');
+        min = min;
+        max = max;
+        secondMax = secondMax;
+        console.log(secondMax);
+      }
     }
-  }
-  // Colocando los valores obtenidos en sus respectivos contenedores
-  maxNumberSpan.textContent = MAX;
-  minNumberSpan.textContent= MIN;
-  secondMaxNumberSpan.textContent = SECONDMAX;
+    // Colocando los valores obtenidos en sus respectivos contenedores
+    maxNumberSpan.textContent = max;
+    minNumberSpan.textContent= min;
+    secondMaxNumberSpan.textContent = secondMax;
+  };
+
+  // Evento click
+  button.addEventListener('click', function() {
+    orderNumbers([45, 75, 200, 256, 1, 27]);
+  });
 });
